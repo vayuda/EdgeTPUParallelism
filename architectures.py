0fbed9ddc7eb2466b8f2cbedcd546e646f6b4493
JayakumarPawan <120random.things@gmail.com>
2024-04-23 17:13:37 -0400

    Added models

--- architectures.py
+++ architectures.py
@@ -15,7 +15,7 @@ def simple():
 def base_conv():
     model = tf.keras.models.Sequential([
 
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 1)),
         tf.keras.layers.DepthwiseConv2D((3, 3), activation='relu'),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
@@ -38,7 +38,7 @@ def base_conv():
 
 def conv_1a():
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256,1)),
         tf.keras.layers.DepthwiseConv2D((3, 3), activation='relu'),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
